280aa1edd901b6f8fb15893d0b3fea947702d8b9
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-09 18:30:35 +0000

    Automatically backed up by Learn

--- test/hoisting-test.js
+++ test/hoisting-test.js
@@ -23,28 +23,28 @@ describe('hoisting', () => {
     })
   })
 
-  // describe('loggers', () => {
-  //   beforeEach(() => {
-  //     chai.spy.on(console, 'log')
-  //   })
-  //
-  //   afterEach(() => {
-  //     console.log.reset()
-  //   })
-  //
-  //   describe('crazy', () => {
-  //     it("prints 'hey!!!' to the console from the nested function", () => {
-  //       crazy()
-  //
-  //       expect(console.log).to.have.been.called.with("hey!!!")
-  //     })
-  //   })
-  //
-  //   describe('sayMyName', () => {
-  //     it("prints 'Kristin' to the console from the nested function", () => {
-  //       sayMyName()
-  //       expect(console.log).to.have.been.called.with("Kristin")
-  //     })
-  //   })
-  // })
+  describe('loggers', () => {
+    beforeEach(() => {
+      chai.spy.on(console, 'log')
+    })
+
+    afterEach(() => {
+      console.log.reset()
+    })
+
+    describe('crazy', () => {
+      it("prints 'hey!!!' to the console from the nested function", () => {
+        crazy()
+
+        expect(console.log).to.have.been.called.with("hey!!!")
+      })
+    })
+
+    describe('sayMyName', () => {
+      it("prints 'Kristin' to the console from the nested function", () => {
+        sayMyName()
+        expect(console.log).to.have.been.called.with("Kristin")
+      })
+    })
+  })
 })

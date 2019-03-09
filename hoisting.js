aca26d73b1976ef84b9c27dfcbed9f24c5029815
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-09 17:40:12 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -8,14 +8,14 @@ function callMe() {
 }
 
 
+var thisIsCrazy = "hey!!!"
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
 
   var thisIsCrazy = function (){
-    console.log("hey!!!")
   }
 }
+  console.log(thisIsCrazy)
 
 function sayMyName() {
   // fix the code in here:
@@ -24,7 +24,7 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
 }
